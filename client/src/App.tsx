950f5c722ad262eca1fc7fa1971ca1a2e02b06c6
oriDEV <ori.nizri.tal@gmail.com>
2025-08-02 20:00:26 +0300

    lazy loading components in app routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,42 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Layout from "./components/Layout/Layout";
-import HomePage from "./pages/HomePage";
-import Profile from "./components/Auth/Profile/Profile";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import Unauthorized from "./pages/Unauthorized";
-import AdminDashboard from "./pages/admin/AdminDashboard";
 import RoleBasedWrapper from "./components/RoleBased/RoleBasedWrapper";
-import TreesDashboard from "./pages/trees/TreeDashboard";
 
-// Main application component that sets up routing and layout
+// Lazy loading components to optimize performance
+const Lazy = {
+  Layout: lazy(() => import("./components/Layout/Layout")),
+  HomePage: lazy(() => import("./pages/HomePage")),
+  Profile: lazy(() => import("./components/Auth/Profile/Profile")),
+  LoginPage: lazy(() => import("./pages/LoginPage")),
+  RegisterPage: lazy(() => import("./pages/RegisterPage")),
+  Unauthorized: lazy(() => import("./pages/Unauthorized")),
+  TreesDashboard: lazy(() => import("./pages/trees/TreeDashboard")),
+};
+
+// Main App component with routing
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
-          {/* Public Routes */}
-          <Route path="/unauthorized" element={<Unauthorized />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<Lazy.Layout />}>
+            {/* Public Routes */}
+            <Route path="/unauthorized" element={<Lazy.Unauthorized />} />
+            <Route path="/login" element={<Lazy.LoginPage />} />
+            <Route path="/register" element={<Lazy.RegisterPage />} />
 
-          {/* Admin Routes */}
-          <Route element={<RoleBasedWrapper allowedRoles={["admin"]} />}>
-            <Route path="/admin-dashboard" element={<AdminDashboard />} />
+            {/* Protected Routes */}
+            <Route
+              element={<RoleBasedWrapper allowedRoles={["user", "admin"]} />}
+            >
+              <Route path="/" element={<Lazy.HomePage />} />
+              <Route path="/profile" element={<Lazy.Profile />} />
+              <Route path="/trees" element={<Lazy.TreesDashboard />} />
+            </Route>
           </Route>
-
-          {/* User Routes */}
-          <Route
-            element={<RoleBasedWrapper allowedRoles={["user", "admin"]} />}
-          >
-            <Route path="/" element={<HomePage />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/trees" element={<TreesDashboard />} />
-          </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { useAuth } from "../contexts/authContext";
+import { Box, Typography, Container } from "@mui/material";
 
 export default function HomePage() {
-  const { user, logout } = useAuth();
+  const { user } = useAuth();
+
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Welcome {user?.first_name}!</h1>
-      <button onClick={logout}>Logout</button>
-    </div>
+    <Container maxWidth="sm">
+      <Box sx={{ padding: 4, textAlign: "center" }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Welcome{user.first_name} {user.last_name}!
+        </Typography>
+      </Box>
+    </Container>
   );
 }
